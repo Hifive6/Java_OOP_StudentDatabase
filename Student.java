933783f50adedf0eb3234d472f37fa19aa679c79
David Juarez <djuar05@gmail.com>
2020-05-22 10:41:14 -0400

    intial files

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String firstName, lastName, classYear, courses;
+    private int studentId, tuitionBalance;
+    private int costOfCourse = 600;
+    
+    public Student(String firstName, String lastName, String classYear){
+
+    }
+}
