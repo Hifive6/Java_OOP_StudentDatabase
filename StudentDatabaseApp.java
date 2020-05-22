c7961b4dbdd1a4d7101e47afc8159060050eea4d
David Juarez <djuar05@gmail.com>
2020-05-22 14:00:29 -0400

    add for loop to create multiple object to create more than one student

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class StudentDatabaseApp{
     public static void main(String[] args) {
         
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.showInfo());
+        //Ask how many new students we aten to add
+        System.out.println("Enter number of Student to enroll: ");
+        Scanner scan = new Scanner(System.in);
+        int numberOfStudents = scan.nextInt();
+        Student[] students = new Student[numberOfStudents];
+
+        //Create n numbers of new students
+        for (int n = 0; n < numberOfStudents; n++){
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+
+        for(int n =0; n<numberOfStudents; n++){
+            System.out.println(students[n].showInfo());
+        }
+        scan.close();
     }
 }
